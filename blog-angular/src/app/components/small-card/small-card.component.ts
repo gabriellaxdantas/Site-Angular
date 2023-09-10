49a02ed2b3ec51f22816e03c76a440b4059f23fa
gabriellaxdantas <gabriellaxdantas@gmail.com>
2023-09-09 21:39:00 -0300

    passando propriedades dinamicamente

--- blog-angular/src/app/components/small-card/small-card.component.ts
+++ blog-angular/src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css', 'small-card.component-responsive.css']
 })
 export class SmallCardComponent implements OnInit {
+  @Input ()
+  cardImgCover:string = "";
+  @Input ()
+  cardTitle:string = "";
 
   constructor() { }
 
